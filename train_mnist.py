c651dcc63f87a38b320ed7b039fcb5e4bc1d9f8b
jtk001 <kadowaki.4@osu.edu>
2020-04-28 23:17:01 -0400

    style update

--- train_mnist.py
+++ train_mnist.py
@@ -115,7 +115,7 @@ def main():
     X_train = preprocess_input(X_train.reshape(X_train.shape[0],-1))
     X_test = preprocess_input(X_test.reshape(X_test.shape[0],-1))
     opt = tf.keras.optimizers.Adam(lr)
-    data = [(X_train, y_train),(X_test, y_test)]
+    data = [(X_train, y_train), (X_test, y_test)]
     if conditional:
         train_conditional(data, opt)
     else:
