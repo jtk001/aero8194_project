4b030d6ce7ae0bcab3c0a63fa6f098b03955b48c
jtk001 <kadowaki.4@osu.edu>
2020-04-28 23:18:53 -0400

    style update

--- vae.py
+++ vae.py
@@ -57,10 +57,10 @@ class ReconstructionLossLayer(tf.keras.layers.Layer):
         x_in, x_recon = inputs
         if not self.bernoulli:
             #from multivariate normal distribution assumption
-            recon_loss = tf.reduce_sum(tf.math.squared_difference(x_in,x_recon),axis=-1)
+            recon_loss = tf.reduce_sum(tf.math.squared_difference(x_in, x_recon),axis=-1)
         else:
             #multivariate bernoulli assumption
-            recon_loss = self.dim*tf.keras.losses.binary_crossentropy(x_in,x_recon)
+            recon_loss = self.dim*tf.keras.losses.binary_crossentropy(x_in, x_recon)
         self.add_loss(tf.reduce_mean(recon_loss))
         return inputs[-1]
     
@@ -108,7 +108,7 @@ class VAE():
         vae_recon = ReconstructionLossLayer(dim=self.input_dim, bernoulli=self.bernoulli)([input_img, vae_recon])
         vae_model = Model(inputs=input_img, outputs=vae_recon,name='vae')
         
-        return encoder,decoder,vae_model
+        return encoder, decoder, vae_model
     
 class CVAE():
 
@@ -151,4 +151,4 @@ class CVAE():
         vae_recon = ReconstructionLossLayer(dim=self.input_dim, bernoulli=self.bernoulli)([input_img, vae_recon])
         vae_model = Model(inputs=[input_img, input_aux_1], outputs=vae_recon, name='cvae')
         
-        return encoder,decoder,vae_model
+        return encoder, decoder, vae_model
