8ec26c0fb1b40e4e29904731e65e558c0ec9d811
jtk001 <kadowaki.4@osu.edu>
2020-04-28 22:59:12 -0400

    fixed style

--- train_mnist.py
+++ train_mnist.py
@@ -57,22 +57,22 @@ def preprocess_input(x):
 
     return x / 255.
 
-def train_conditional(data,optimizer):
+def train_conditional(data, optimizer):
 
-    (X_train,y_train),(X_test,y_test) = data
+    (X_train, y_train), (X_test, y_test) = data
     lb = LabelBinarizer()
     y_train = lb.fit_transform(y_train)
     y_test = lb.transform(y_test)
-    fname = os.path.join(data_dir,'cvae_train.csv')
+    fname = os.path.join(data_dir, 'cvae_train.csv')
     logger = tf.keras.callbacks.CSVLogger(filename=fname)
-    encoder,decoder,cvae = CVAE(input_dim=X_train.shape[-1],
+    encoder, decoder, cvae = CVAE(input_dim=X_train.shape[-1],
                                 latent_dim=latent_dim,
                                 aux_dim=y_train.shape[-1],
                                 beta=beta,
                                 output_activation='sigmoid').build()
     print(cvae.summary())
-    cvae.compile(optimizer,loss=None)
-    cvae.fit(x=[X_train,y_train],
+    cvae.compile(optimizer, loss=None)
+    cvae.fit(x=[X_train, y_train],
              y=None,
              validation_data=([X_test,y_test],None),
              epochs=epochs,
@@ -81,45 +81,45 @@ def train_conditional(data,optimizer):
              verbose=2,
              callbacks=[logger])
     print('\nfinished training, saving models...')
-    cvae.save(os.path.join(model_dir,'cvae.h5'))
-    encoder.save(os.path.join(model_dir,'conditional_encoder.h5'))
-    decoder.save(os.path.join(model_dir,'conditional_decoder.h5'))
+    cvae.save(os.path.join(model_dir, 'cvae.h5'))
+    encoder.save(os.path.join(model_dir, 'conditional_encoder.h5'))
+    decoder.save(os.path.join(model_dir, 'conditional_decoder.h5'))
     
 def train_vanilla(data,optimizer):
 
-    (X_train,_),(X_test,_) = data
-    fname = os.path.join(data_dir,'vae_train.csv')
+    (X_train, _), (X_test, _) = data
+    fname = os.path.join(data_dir, 'vae_train.csv')
     logger = tf.keras.callbacks.CSVLogger(filename=fname)
-    encoder,decoder,vae = VAE(input_dim=X_train.shape[-1],
-                              latent_dim=latent_dim,
-                              beta=beta,
-                              output_activation='sigmoid').build()
+    encoder, decoder, vae = VAE(input_dim=X_train.shape[-1],
+                                latent_dim=latent_dim,
+                                beta=beta,
+                                output_activation='sigmoid').build()
     print(vae.summary())
-    vae.compile(optimizer,loss=None)
+    vae.compile(optimizer, loss=None)
     vae.fit(x=X_train,
             y=None,
-            validation_data=(X_test,None),
+            validation_data=(X_test, None),
             epochs=epochs,
             batch_size=bs,
             shuffle=True,
             verbose=2,
             callbacks=[logger])
     print('\nfinished training, saving models...')
-    vae.save(os.path.join(model_dir,'vae.h5'))
-    encoder.save(os.path.join(model_dir,'encoder.h5'))
-    decoder.save(os.path.join(model_dir,'decoder.h5'))
+    vae.save(os.path.join(model_dir, 'vae.h5'))
+    encoder.save(os.path.join(model_dir, 'encoder.h5'))
+    decoder.save(os.path.join(model_dir, 'decoder.h5'))
     
 def main():
 
-    (X_train,y_train), (X_test,y_test) = tf.keras.datasets.mnist.load_data()
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
     X_train = preprocess_input(X_train.reshape(X_train.shape[0],-1))
     X_test = preprocess_input(X_test.reshape(X_test.shape[0],-1))
     opt = tf.keras.optimizers.Adam(lr)
-    data = [(X_train,y_train),(X_test,y_test)]
+    data = [(X_train, y_train),(X_test, y_test)]
     if conditional:
-        train_conditional(data,opt)
+        train_conditional(data, opt)
     else:
-        train_vanilla(data,opt)
+        train_vanilla(data, opt)
     
 if __name__ == '__main__':
     main()
--- vae.py
+++ vae.py
@@ -26,19 +26,19 @@ class SamplingLayer(tf.keras.layers.Layer):
     
 class KLDivergenceLayer(tf.keras.layers.Layer):
 
-    def __init__(self,beta=0.5,**kwargs):
+    def __init__(self, beta=0.5, **kwargs):
         self.beta = beta
         self.is_placeholder = True
         super().__init__(**kwargs)
     
-    def call(self,inputs):
+    def call(self, inputs):
         z_mu, z_log_sigma = inputs
         z_var = tf.math.square(tf.math.exp(z_log_sigma))
         kl_loss = self.beta * tf.reduce_sum(tf.square(z_mu) + z_var - 2*z_log_sigma - 1,axis=-1)
         self.add_loss(tf.reduce_mean(kl_loss))
         return inputs
     
-    def compute_output_shape(self,input_shape):
+    def compute_output_shape(self, input_shape):
         return input_shape
     
     def get_config(self):
@@ -48,12 +48,12 @@ class KLDivergenceLayer(tf.keras.layers.Layer):
     
 class ReconstructionLossLayer(tf.keras.layers.Layer):
 
-    def __init__(self,dim,bernoulli=False,**kwargs):
+    def __init__(self, dim, bernoulli=False, **kwargs):
         self.dim = dim
         self.bernoulli = bernoulli
         super().__init__(**kwargs)
     
-    def call(self,inputs):
+    def call(self, inputs):
         x_in, x_recon = inputs
         if not self.bernoulli:
             #from multivariate normal distribution assumption
@@ -64,7 +64,7 @@ class ReconstructionLossLayer(tf.keras.layers.Layer):
         self.add_loss(tf.reduce_mean(recon_loss))
         return inputs[-1]
     
-    def compute_output_shape(self,input_shape):
+    def compute_output_shape(self, input_shape):
         return input_shape[-1]
     
     def get_config(self):
@@ -89,24 +89,24 @@ class VAE():
             self.bernoulli = False
         
     def build(self):
-        input_img = Input(self.input_dim,name='image-input')
-        x = Dense(256,activation='relu',name='hidden-downsample-1')(input_img)
-        x = Dense(64,activation='relu',name='hidden-downsample-2')(x)
-        z_mu = Dense(self.latent_dim,activation=None,name='z-mean')(x)
-        z_log_sigma = Dense(self.latent_dim,activation=None,name='z-log-sigma')(x)
-        z_mu, z_log_sigma = KLDivergenceLayer(beta=self.beta)([z_mu,z_log_sigma])
-        encoder = Model(inputs=input_img,outputs=[z_mu,z_log_sigma],name='encoder')
-        z = SamplingLayer(name='reparametrization-trick-sampling')([z_mu,z_log_sigma])
+        input_img = Input(self.input_dim, name='image-input')
+        x = Dense(256, activation='relu', name='hidden-downsample-1')(input_img)
+        x = Dense(64, activation='relu', name='hidden-downsample-2')(x)
+        z_mu = Dense(self.latent_dim, activation=None, name='z-mean')(x)
+        z_log_sigma = Dense(self.latent_dim, activation=None, name='z-log-sigma')(x)
+        z_mu, z_log_sigma = KLDivergenceLayer(beta=self.beta)([z_mu, z_log_sigma])
+        encoder = Model(inputs=input_img, outputs=[z_mu, z_log_sigma], name='encoder')
+        z = SamplingLayer(name='reparametrization-trick-sampling')([z_mu, z_log_sigma])
         
         latent_input = Input(self.latent_dim)
-        x = Dense(64,activation='relu',name='hidden-upsample-1')(latent_input)
-        x = Dense(256,activation='relu',name='hidden-upsample-2')(x)
-        x = Dense(self.input_dim,activation=self.output_activation,name='reconstructed-image')(x)
-        decoder = Model(inputs=latent_input,outputs=x,name='decoder')
+        x = Dense(64, activation='relu', name='hidden-upsample-1')(latent_input)
+        x = Dense(256, activation='relu', name='hidden-upsample-2')(x)
+        x = Dense(self.input_dim, activation=self.output_activation, name='reconstructed-image')(x)
+        decoder = Model(inputs=latent_input, outputs=x, name='decoder')
         
         vae_recon = decoder(z)
-        vae_recon = ReconstructionLossLayer(dim=self.input_dim,bernoulli=self.bernoulli)([input_img,vae_recon])
-        vae_model = Model(inputs=input_img,outputs=vae_recon,name='vae')
+        vae_recon = ReconstructionLossLayer(dim=self.input_dim, bernoulli=self.bernoulli)([input_img, vae_recon])
+        vae_model = Model(inputs=input_img, outputs=vae_recon,name='vae')
         
         return encoder,decoder,vae_model
     
@@ -128,27 +128,27 @@ class CVAE():
             self.bernoulli = False
         
     def build(self):
-        input_img = Input(self.input_dim,name='image-input')
-        input_aux_1 = Input(self.aux_dim,name='aux-input-1')
-        x = Dense(256,activation='relu',name='hidden-downsample-1')(input_img)
-        x = Concatenate(name='merge-inputs')([x,input_aux_1])
-        x = Dense(64,activation='relu',name='hidden-downsample-2')(x)
-        z_mu = Dense(self.latent_dim,activation=None,name='z-mean')(x)
-        z_log_sigma = Dense(self.latent_dim,activation=None,name='z-log-sigma')(x)
-        z_mu, z_log_sigma = KLDivergenceLayer(beta=self.beta)([z_mu,z_log_sigma])
-        encoder = Model(inputs=[input_img,input_aux_1],outputs=[z_mu,z_log_sigma],name='encoder')
-        z = SamplingLayer(name='reparametrization-trick-sampling')([z_mu,z_log_sigma])
+        input_img = Input(self.input_dim, name='image-input')
+        input_aux_1 = Input(self.aux_dim, name='aux-input-1')
+        x = Dense(256,activation='relu', name='hidden-downsample-1')(input_img)
+        x = Concatenate(name='merge-inputs')([x, input_aux_1])
+        x = Dense(64,activation='relu', name='hidden-downsample-2')(x)
+        z_mu = Dense(self.latent_dim, activation=None, name='z-mean')(x)
+        z_log_sigma = Dense(self.latent_dim, activation=None, name='z-log-sigma')(x)
+        z_mu, z_log_sigma = KLDivergenceLayer(beta=self.beta)([z_mu, z_log_sigma])
+        encoder = Model(inputs=[input_img, input_aux_1], outputs=[z_mu, z_log_sigma],name='encoder')
+        z = SamplingLayer(name='reparametrization-trick-sampling')([z_mu, z_log_sigma])
         
         latent_input = Input(self.latent_dim)
-        input_aux_2 = Input(self.aux_dim,name='aux-input-2')
-        x = Dense(64,activation='relu',name='hidden-upsample-1')(latent_input)
-        x = Concatenate(name='merge-inputs')([x,input_aux_2])
-        x = Dense(256,activation='relu',name='hidden-upsample-2')(x)
-        x = Dense(self.input_dim,activation=self.output_activation,name='reconstructed-image')(x)
-        decoder = Model(inputs=[latent_input,input_aux_2],outputs=x,name='decoder')
+        input_aux_2 = Input(self.aux_dim, name='aux-input-2')
+        x = Dense(64, activation='relu', name='hidden-upsample-1')(latent_input)
+        x = Concatenate(name='merge-inputs')([x, input_aux_2])
+        x = Dense(256, activation='relu', name='hidden-upsample-2')(x)
+        x = Dense(self.input_dim, activation=self.output_activation, name='reconstructed-image')(x)
+        decoder = Model(inputs=[latent_input, input_aux_2], outputs=x, name='decoder')
         
-        vae_recon = decoder([z,input_aux_1])
-        vae_recon = ReconstructionLossLayer(dim=self.input_dim,bernoulli=self.bernoulli)([input_img,vae_recon])
-        vae_model = Model(inputs=[input_img,input_aux_1],outputs=vae_recon,name='cvae')
+        vae_recon = decoder([z, input_aux_1])
+        vae_recon = ReconstructionLossLayer(dim=self.input_dim, bernoulli=self.bernoulli)([input_img, vae_recon])
+        vae_model = Model(inputs=[input_img, input_aux_1], outputs=vae_recon, name='cvae')
         
         return encoder,decoder,vae_model
